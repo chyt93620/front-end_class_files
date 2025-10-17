3b8d474e04fca393cd66fe7a838a9aad55744c08
chyt93620 <chyt93620@gmail.com>
2025-10-17 12:31:28 +0800

    checkbox

--- JS/aa.js
+++ JS/aa.js
@@ -1,2 +1,25 @@
 //註解 comment
 //變數 variable
+
+// 判段單數
+
+// const number = 5;
+
+// if (number % 2 != 0) {
+//   console.log(number + "是單數");
+// } else {
+//   console.log(number + "是雙數");
+// }
+
+// let i;
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("hello");
+// }
+
+// console.log(i);
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
