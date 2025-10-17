1dee9c092501a4a29392a145afef6abfb4fc6f03
chyt93620 <chyt93620@gmail.com>
2025-10-17 16:30:45 +0800

    調整樣式

--- JS/aa.js
+++ JS/aa.js
@@ -18,8 +18,61 @@
 // }
 
 // console.log(i);
-for (let i = 0; i < 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i);
-  }
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i = i++;
+// }
+
+//九九乘法表
+
+// for (let i = 1; i < 10; i++) {
+//   for (let j = 1; j < 10; j++) {
+//     console.log(i + " x " + j + " = " + i * j);
+//   }
+// }
+// for (let i = 1; i < 10; i++) {
+//   for (let j = 1; j < 10; j++) {
+//     const result = " ${i} x ${j} =${i*j} ";
+//     console.log(result);
+//   }
+// }
+
+// for (i = 0; i < 6; i++) {
+//   console.log("*".repeat(i));
+// }
+
+//星星樹
+
+//for (i = 1; i < 6; i++) {
+//   console.log(" ".repeat(5-i) + "*".repeat(i * 2 - 1));
+// }
+
+// function sayHelloTo(someone = "john") {
+//   console.log(someone);
+// }
+
+// sayHelloTo();
+// sayHelloTo("cc");
+
+// function pay() {
+//   const price = 50;
+//   const change = n - price;
+
+//   return change;
+// }
+
+function calcBMI(height, weight) {
+  const h = height / 100;
+  const result = weight / (h * h);
+  return result;
 }
+
+const bmi = calcBMI(170, 64);
+console.log(bmi);
