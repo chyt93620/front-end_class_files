b1984f3a7f3fe71b99c4f0a85015f630943074ce
chyt93620 <chyt93620@gmail.com>
2025-11-03 11:46:34 +0800

    js scope

---
+++ JS/1103/aa.js
@@ -0,0 +1,21 @@
+// function hey() {
+//   console.log("hey");
+// }
+
+// function hello() {
+//   console.log("hello");
+// }
+
+// function world() {
+//   console.log("world");
+// }
+
+// let x = 1;
+// function a() {
+//   x++;
+//   console.log(x);
+//   a();
+// }
+
+// a();
+
