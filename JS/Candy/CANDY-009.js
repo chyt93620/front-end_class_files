220a51da1f286f6b140fda6d2fea7615c636785f
chyt93620 <235834389+chyt93620@users.noreply.github.com>
2025-11-16 01:11:18 +0800

    CANDY009

--- JS/Candy/CANDY-009.js
+++ JS/Candy/CANDY-009.js
@@ -2,8 +2,21 @@
 // 程式語言：JavaScript
 // 題目：移除網址中的錨點（Anchor）
 
+// 可讀性較高，但比較長
 function removeAnchor(url) {
-  // 實作寫在這裡
+  const ifAnchor = url.indexOf("#");
+
+  if (ifAnchor == -1) {
+    return url;
+  } else {
+    return url.slice(0, ifAnchor);
+  }
+}
+
+// 比較短，功能完全相同，可讀性稍低
+function removeAnchor(url) {
+  const ifAnchor = url.indexOf("#");
+  return ifAnchor === -1 ? url : url.slice(0, ifAnchor);
 }
 
 console.log(removeAnchor("5xcampus.com")); // 印出 5xcampus.com
